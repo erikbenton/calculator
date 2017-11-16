230cdc899288989460ed58a52ce76dee40f24a8c
erikbenton <erik.benton@hotmail.com>
2017-11-16 14:52:46 -0500

    adding opacity change when mouse enters a square

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,43 +1,117 @@
 
 let container = document.querySelector(".container");
+let clearButton = document.querySelector(".clear-btn");
 let squares;
+let rows;
 
-function initialize(rows = 16, cols = 16)
+function initialize(numberOfRows = 16, numberOfCols = 16)
 {
-	for(let i = 0; i < rows; i++)
-	{
-		container.innerHTML += `<div class="row">`;
-		for(let j = 0; j < cols; j++)
-		{
-			container.innerHTML += `<div class="square">.</div>`;
-		}
-		container.innerHTML += `</div>`;
-	}
-
-	squares = document.querySelectorAll(".square");
-
-	squares.forEach(function(square)
-	{
-		square.style.height = 960/cols + "px";
-		square.style.width = 960/rows + "px";
-
-		// square.addEventListener("mouseover", function(e)
-		// {
-		// 	console.log("Here");
-		// 	square.classList.add("black-square");
-		// });
-
-	});
-
-	container.addEventListener("mouseover", function(e)
-	{
-		let red = Math.floor(Math.random() * 255 + 1) + "";
-		let green = Math.floor(Math.random() * 255 + 1) + "";
-		let blue = Math.floor(Math.random() * 255 + 1) + "";
-		e.target.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
-		e.target.style.color = "rgb(" + red + ", " + green + ", " + blue + ")";
-	});
-
+	setupGrid(numberOfRows, numberOfCols);
+	squaresEvents();
+	clearButtonEvent();
 }
 
-initialize(50, 50);
+function clearButtonEvent()
+{
+	clearButton.addEventListener("click", function(e)
+	{
+		
+		let size = getGridSize();
+		
+		setupGrid(size, size);
+	});
+}
+
+function getGridSize()
+{
+	let size;
+
+		do
+		{
+			size = parseInt(prompt("What size do you want the new grid to be?", 16));
+			
+			if(size > 100)
+			{
+				alert("Please enter a smaller integer (<100)...")
+			}
+			if(size < 1)
+			{
+				alert("Please enter a positive integer...")
+			}
+
+		} while (size > 100 || size < 1);
+
+		return size;
+}
+
+function squaresEvents()
+{
+	container.addEventListener("mouseover", function(e)
+	{
+
+		if(!e.target.classList.contains("square")) return;
+		
+		let squareOpacity = Number.parseFloat(e.target.style.opacity);
+
+		if(e.target.classList.contains("marked")) return;
+
+		e.target.style.opacity = (squareOpacity + 0.1) + "";
+
+		if(squareOpacity >= 0.9)
+		{
+			e.target.classList.add("marked");
+		}
+		
+	});
+}
+
+function setupGridHTML(numberOfRows, numberOfCols)
+{
+	container.innerHTML = "";
+
+	let htmlForDivs = "";
+
+	for(let i = 0; i < numberOfRows; i++)
+	{
+		htmlForDivs += `<div class="row">`;
+		for(let j = 0; j < numberOfCols; j++)
+		{
+			htmlForDivs += `<div class="square"></div>`;
+		}
+		htmlForDivs += `</div>`;
+	}
+
+	container.innerHTML = htmlForDivs;
+}
+
+function setupGrid(numberOfRows, numberOfCols)
+{
+	setupGridHTML(numberOfRows, numberOfCols);
+	setupSquares(numberOfRows, numberOfCols);
+	setupRows(numberOfRows, numberOfCols);
+}
+
+function setupSquares(numberOfRows, numberOfCols)
+{
+	squares = document.querySelectorAll(".square");
+	
+	squares.forEach(function(square)
+	{
+		square.style.height = 960/numberOfCols + "px";
+		square.style.width = 960/numberOfRows + "px";
+		square.style.opacity = "0";
+		square.style.backgroundColor = "rgb(0,0,0)";
+
+	});
+}
+
+function setupRows(numberOfRows, numberOfCols)
+{
+	rows = document.querySelectorAll(".row");
+	rows.forEach(function(row)
+	{
+		row.style.height = 960/numberOfRows + "px";
+	});
+}
+
+initialize();
